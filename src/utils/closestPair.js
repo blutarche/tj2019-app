8c025d0151d40342dcbac8f33803515fbfd9f547
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2019-11-30 18:04:42 +0700

    ⚡️ update closest pair

--- src/utils/closestPair.js
+++ src/utils/closestPair.js
@@ -12,10 +12,10 @@ const bruteForce = (positions = []) => {
   return min
 }
 
-const stripClosest = (positions = [], d) => {
+const stripClosest = (strip = [], d) => {
   let min = d
-  const size = positions.length
-  const strip = _.sortBy(positions, ['x', 'y'])
+  const size = strip.length
+  // const strip = _.sortBy(positions, ['x', 'y'])
 
   for (let i = 0; i < size; ++i)
     for (let j = i + 1; j < size && strip[j].y - strip[i].y < min; ++j)
@@ -25,27 +25,36 @@ const stripClosest = (positions = [], d) => {
   return min
 }
 
-const closestUtil = (positions = []) => {
-  const n = positions.length
-  if (n <= 3) return bruteForce(positions)
+const closestUtil = (posXs = [], posYs = []) => {
+  const n = posXs.length
+  if (n <= 3) return bruteForce(posXs)
 
   const mid = Math.floor(n / 2)
-  const midPoint = positions[mid]
+  const midPoint = posXs[mid]
 
-  const dl = closestUtil(positions.slice(0, mid))
-  const dr = closestUtil(positions.slice(mid))
+  const pyl = []
+  const pyr = []
+  for (let i = 0; i < n; i++) {
+    if (posYs[i].x <= midPoint.x) pyl.push(posYs[i])
+    else pyr.push(posYs[i])
+  }
+
+  const dl = closestUtil(posXs.slice(0, mid), pyl.slice(0, mid))
+  const dr = closestUtil(posXs.slice(mid), pyr.slice(mid))
 
   const d = Math.min(dl, dr)
 
   const strip = []
   for (let i = 0; i < n; i++)
-    if (Math.abs(positions[i].x - midPoint.x) < d) strip.push(positions[i])
+    if (Math.abs(posYs[i].x - midPoint.x) < d) strip.push(posYs[i])
 
   return Math.min(d, stripClosest(strip, d))
 }
 
 const calculateClosestPair = (positions = []) => {
-  positions = _.sortBy(positions, ['x', 'y'])
-  return closestUtil(positions, positions.length)
+  const posXs = _.sortBy(positions, ['x'])
+  const posYs = _.sortBy(positions, ['y'])
+
+  return closestUtil(posXs, posYs)
 }
 exports.calculateClosestPair = calculateClosestPair
