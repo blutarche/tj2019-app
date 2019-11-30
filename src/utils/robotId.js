87fb2884915d69a72279ba66ffc8b5615857274c
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2019-11-30 18:56:22 +0700

    ✨ validate robot id

--- src/utils/robotId.js
+++ src/utils/robotId.js
@@ -2,7 +2,9 @@ const _ = require('lodash')
 
 const regexRobotId = /^robot[#]([1-9][0-9]*)$/
 exports.regexRobotId = regexRobotId
+const regexRobotOnlyID = /^[1-9][0-9]*$/
 exports.getRobotId = robotId => {
+  if (!regexRobotOnlyID.test(robotId)) return null
   const robotIdAsNumber = _.toNumber(robotId)
   if (_.isNaN(robotIdAsNumber)) {
     return null
