cc508f97b71c29b962eea7605d024a69cba3ecd7
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2019-11-30 15:51:48 +0700

    ✨ private

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const { router: robotRouter } = require('./robots/main')
+const { router: privateRouter } = require('./private/main')
 
 const app = express()
 app.use(cors())
 const port = process.env.PORT || 4000
 
 app.use('/', robotRouter)
+app.use('/private', privateRouter)
 
 app.listen(port, () => {
   console.log(`🚀 Listening on port ${port}`)
